9f3f048b4f11a9c58149e4a623963d133181b4db
Peliorange <jen.celedon@duocuc.cl>
2025-09-07 17:09:03 -0300

    actualizando crud

--- crudProject/tasks/serializers.py
+++ crudProject/tasks/serializers.py
@@ -3,7 +3,9 @@ from .models import Tasks
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    creador = serializers.PrimaryKeyRelatedField(read_only=True)
+    creador = serializers.PrimaryKeyRelatedField(source="creador.id", read_only=True, allow_null=True)
+
     class Meta:
         model = Tasks
         fields = "__all__"
+        extra_fields = [ "creador" ]
